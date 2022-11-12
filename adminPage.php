9a35116dfdfc045d6c3673be2f821cc797726d87
HellooZC <116249540+HellooZC@users.noreply.github.com>
2022-11-12 14:55:13 +0800

    Changes to the AdminHomePage

--- adminPage.php
+++ adminPage.php
@@ -3,17 +3,16 @@
 
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/admin.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Share Skill</title>
 </head>
 <body>
     <header>
-        <div class="logo">
-            <a href="index.html"><img src="images/ss.png" alt="Logo" title="Logo"></a>
-        </div>
+	
 
-        <nav>
+        <nav class="header">
+		<div><img class="Logo" src="images/Logo.png"></div>
             <ul>
                 <li><a href="banneradsui.php">Banner</a></li>
                 <li><a href="enhancement.html">Booking</a></li>
@@ -22,8 +21,19 @@
         </nav>
 
     </header>
-
-
+	
+	<div class="Flower">
+        <div class="container">
+            <div class="container-homepage1">
+                <div>
+                    <p class="welcome">Welcome to Cacti-Succulent</p>
+                </div>
+                <div>
+                    <p class="purpose">Cacti-Succulent Kuching is a local homegrown business specialized in selling various type and size of succulent plants. Apart from selling succulent plants, we also sell different type of gardening tools, soils and fertilizers at an affordable cost.</p>
+                </div>
+            </div>
+        </div>
+    </div> 
 	<section class="banner-service">
         <h1>Report</h1>
 		<aside class="intro">
@@ -49,6 +59,21 @@
 		</div>
     </aside>
     </section>
+	<footer>
+        <section class=" footer">
+
+
+            <div class="section-4">
+                <ul class="footer-links">
+                    <li><a href="">Contact Us</a></li>
+                    <li><a href="">Location</a></li>
+                </ul>
+            </div>
+            <iframe class="Map" src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3985.850742379384!2d101.80436031429385!3d2.555405357229252!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMsKwMzMnMTkuNCJOIDEwMcKwNDgnMjMuNiJF!5e0!3m2!1sen!2smy!4v1666693025798!5m2!1sen!2smy" ></iframe>>
+
+
+        </section>
+    </footer>
 </body>
 
 </html>
